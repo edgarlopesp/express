2dd129834e523cb291d4f9f79a7d4885ea398fb7
edgar lopez <edgarlopesp@gmail.com>
2022-05-11 18:57:13 -0600

    modificacion de app.js

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var app= express();
+const express = require('express');
+const port =3002;
+const bodyParse = require('body-parse');
+const routes = require('./routes/routes');
+const { error } = require('console');
+const app = express();
 
-app.get('/',function(req, res){
-    res.send('Hola mundo!');
-});
-app.listen(3000,function(){
-    console.log('Aplicacion ejemplo, escuchando el puerto 3000!');
-});
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({
+    extended: true,
+}));
+
+routes(app);
+
+const server = app.listen(port,(error) =>{
+    if(error) return console.log(`Error: ${error}`);
+    console.log(`EL servidor escucha en el puerto ${server.address().port}`)
+}
+);
